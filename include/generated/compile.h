f851708e65c7c1b17031d16c9a3d2e61a9cd3177
Echoa89 <echoa8906@gmail.com>
2015-01-06 17:10:51 -0600

    source push

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 27 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#27 SMP PREEMPT Mon Dec 29 19:20:50 CST 2014"
+#define LINUX_COMPILE_BY "echoa"
+#define LINUX_COMPILE_HOST "Toxic-L"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
